2fb6f3a2fd0a3d8e43a32fd42545421524bff9e5
denis <denis.s.sokolov@gmail.com>
2025-07-24 14:28:07 +0300

    some subscriber registration tests

--- demo/S1apDb.cpp
+++ demo/S1apDb.cpp
@@ -175,17 +175,22 @@ auto S1apDb::handleIdentityResponse(const Event& aEvent) -> std::optional<S1apOu
 
 	if(auto subscriber = m_subscribers.find(aEvent.imsi.value()); m_subscribers.end() != subscriber)
 	{
-		subscriber->second.lastActiveTimestamp = aEvent.timestamp;
-
 		if(!subscriber->second.waitingForIdentityResponse)
+		{
+			subscriber->second.lastActiveTimestamp = aEvent.timestamp;
 			return std::nullopt;
+		}
 
 		subscriber->second.waitingForIdentityResponse = false;
 
 		// check request timeout condition
 		if(aEvent.timestamp - subscriber->second.lastActiveTimestamp > request_timeout_1_sec_ms)
+		{
+			subscriber->second.lastActiveTimestamp = aEvent.timestamp;
 			return std::nullopt;
+		}
 
+		subscriber->second.lastActiveTimestamp = aEvent.timestamp;
 		subscriber->second.cgi = aEvent.cgi.value();
 
 		// TODO: return struct on change cgi or always?
--- demo/tests/test_1.cpp
+++ demo/tests/test_1.cpp
@@ -1,24 +1,7 @@
 #include <gtest/gtest.h>
 #include "..\S1apDb.h"
 
-//class MyTest : public ::testing::Test
-//{
-//public:
-//protected:
-//	static void SetUpTestCase()
-//	{
-//		std::cout << "TestSuiteSetup" << std::endl;
-//	}
-//
-//	static void TearDownTestCase()
-//	{
-//
-//	}
-//};
-
-demo::S1apDb foo;
-
-TEST(ExampleTest, BasicAssertions)
+TEST(Registration_Test, eNodeB_AttachRequest_imsi)
 {
 	demo::Event event {
 			.timestamp = 0ull,
@@ -27,6 +10,8 @@ TEST(ExampleTest, BasicAssertions)
 			.imsi = 100ull,
 			.cgi = {{0, 1, 2, 3}}
 	};
+
+	demo::S1apDb foo;
 	const auto res = foo.handler(event);
 
 	const demo::S1apOut out {
@@ -37,3 +22,204 @@ TEST(ExampleTest, BasicAssertions)
 
 	EXPECT_EQ(res.value(), out);
 }
+
+TEST(Registration_Test, eNodeB_AttachRequest_imsi_same_message)
+{
+	demo::Event event{
+			.timestamp = 10ull,
+			.event_type = demo::Event::EventType::AttachRequest,
+			.enodeb_id = 0,
+			.imsi = 100ull,
+			.cgi = {{0, 1, 2, 3}}
+	};
+
+	demo::S1apDb foo;
+	foo.handler(event);
+	const auto res = foo.handler(event);
+
+	EXPECT_EQ(res, std::nullopt);
+}
+
+TEST(Registration_Test, eNodeB_AttachRequest_new_m_tmsi_new_subs_no_history)
+{
+	demo::Event event{
+			.timestamp = 10ull,
+			.event_type = demo::Event::EventType::AttachRequest,
+			.enodeb_id = 0,
+			.m_tmsi = 100,
+			.cgi = {{0, 1, 2, 3}}
+	};
+
+	demo::S1apDb foo;
+	foo.handler(event);
+	const auto res = foo.handler(event);
+
+	EXPECT_EQ(res, std::nullopt);
+}
+
+TEST(Registration_Test, eNodeB_AttachRequest_old_m_tmsi_old_subs_with_history)
+{
+	demo::Event event{
+			.timestamp = 0ull,
+			.event_type = demo::Event::EventType::AttachRequest,
+			.enodeb_id = 10,
+			.imsi = 100ull,
+			.cgi = {{0, 1, 2, 3}}
+	};
+
+	demo::S1apDb foo;
+	foo.handler(event);
+	event = demo::Event {
+			.timestamp = 900ull,
+			.event_type = demo::Event::EventType::AttachAccept,
+			.enodeb_id = 10,
+			.mme_id = 10,
+			.m_tmsi = 100,
+	};
+	foo.handler(event);
+
+	event = demo::Event {
+		.timestamp = 1000ull,
+		.event_type = demo::Event::EventType::AttachRequest,
+		.enodeb_id = 20,
+		.m_tmsi = 100,
+		.cgi = {{0, 1, 2, 3}}
+	};
+	const auto res = foo.handler(event);
+
+	EXPECT_EQ(res, std::nullopt);
+}
+
+TEST(Registration_Test, eNodeB_AttachAccept_imsi_new_subs_less_1_sec)
+{
+	demo::Event event{
+			.timestamp = 0ull,
+			.event_type = demo::Event::EventType::AttachRequest,
+			.enodeb_id = 10,
+			.imsi = 100ull,
+			.cgi = {{0, 1, 2, 3}}
+	};
+
+	demo::S1apDb foo;
+	foo.handler(event);
+	event = demo::Event{
+			.timestamp = 900ull,
+			.event_type = demo::Event::EventType::AttachAccept,
+			.enodeb_id = 10,
+			.mme_id = 10,
+			.m_tmsi = 100,
+	};
+	const auto res = foo.handler(event);
+
+	const demo::S1apOut out{
+				.s1ap_type = demo::S1apOut::Reg,
+				.imsi = 100ull,
+				.cgi = {{0, 1, 2, 3}}
+	};
+
+	EXPECT_EQ(res.value(), out);
+}
+
+TEST(Registration_Test, eNodeB_AttachAccept_imsi_new_subs_more_1_sec)
+{
+	demo::Event event{
+			.timestamp = 0ull,
+			.event_type = demo::Event::EventType::AttachRequest,
+			.enodeb_id = 10,
+			.imsi = 100ull,
+			.cgi = {{0, 1, 2, 3}}
+	};
+
+	demo::S1apDb foo;
+	foo.handler(event);
+	event = demo::Event{
+			.timestamp = 1100ull,
+			.event_type = demo::Event::EventType::AttachAccept,
+			.enodeb_id = 10,
+			.mme_id = 10,
+			.m_tmsi = 100,
+	};
+	const auto res = foo.handler(event);
+
+	EXPECT_EQ(res, std::nullopt);
+}
+
+TEST(Registration_Test, eNodeB_AttachRequest_Identity_Respond_less_1_sec)
+{
+	demo::Event event{
+		.timestamp = 0ull,
+		.event_type = demo::Event::EventType::AttachRequest,
+		.enodeb_id = 10,
+		.imsi = 100ull,
+		.cgi = {{0, 1, 2, 3}}
+	};
+
+	demo::S1apDb foo;
+	foo.handler(event);
+	event = demo::Event{
+			.timestamp = 900ull,
+			.event_type = demo::Event::EventType::AttachAccept,
+			.enodeb_id = 10,
+			.mme_id = 10,
+			.m_tmsi = 100,
+	};
+	foo.handler(event);
+
+	event = demo::Event {
+			.timestamp = 920ull,
+			.event_type = demo::Event::EventType::IdentityResponse,
+			.enodeb_id = 0,
+			.mme_id = 0,
+			.imsi = 100ull,
+			.cgi = {{0, 1, 2, 3}}
+	};
+	const auto res = foo.handler(event);
+
+	const demo::S1apOut out{
+			.s1ap_type = demo::S1apOut::Cgi,
+			.imsi = 100ull,
+			.cgi = {{0, 1, 2, 3}}
+	};
+
+	EXPECT_EQ(res.value(), out);
+}
+
+TEST(Registration_Test, eNodeB_AttachRequest_Identity_Respond_more_1_sec)
+{
+	demo::Event event{
+		.timestamp = 0ull,
+		.event_type = demo::Event::EventType::AttachRequest,
+		.enodeb_id = 10,
+		.imsi = 100ull,
+		.cgi = {{0, 1, 2, 3}}
+	};
+
+	demo::S1apDb foo;
+	foo.handler(event);
+	event = demo::Event{
+			.timestamp = 900ull,
+			.event_type = demo::Event::EventType::AttachAccept,
+			.enodeb_id = 10,
+			.mme_id = 10,
+			.m_tmsi = 100,
+	};
+	foo.handler(event);
+
+	event = demo::Event{
+			.timestamp = 1020ull,
+			.event_type = demo::Event::EventType::IdentityResponse,
+			.enodeb_id = 0,
+			.mme_id = 0,
+			.imsi = 100ull,
+			.cgi = {{0, 1, 2, 3}}
+	};
+	const auto res = foo.handler(event);
+
+	EXPECT_EQ(res, std::nullopt);
+}
+
+
+//TEST(Deregistration_Test, eNodeB_AttachRequest_Identity_Respond)
+//TEST(PathChanging_Test, eNodeB_AttachRequest_Identity_Respond)
+//TEST(Timeouts_Test, eNodeB_AttachRequest_Identity_Respond)
+//TEST(CallFlow_Test, eNodeB_AttachRequest_Identity_Respond)
